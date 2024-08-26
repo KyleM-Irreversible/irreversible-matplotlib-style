68b0e7a35ed4403f6b57cb7a3970f9620de427e0
Kyle Mills <kyle.mills@1qbit.com>
2024-08-26 15:20:11 +0000

    fixed one last fuchsia typo

--- plotirr/__init__.py
+++ plotirr/__init__.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as pyplot
 import plotirr.initiate
-from plotirr.initiate import rainbow, purple, magenta, fucsia, default
+from plotirr.initiate import rainbow, purple, magenta, fuchsia, default
 from plotirr.util import plot_to_gif
 import plotirr.report_maker 
